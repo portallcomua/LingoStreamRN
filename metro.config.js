fc789e9bfd9aceff0ffdd3cb786f432e8ccd7ada
portallcomua <41564292+portallcomua@users.noreply.github.com>
2026-07-02 19:10:22 +0300

    Add metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  project: {
+    ios: {},
+    android: {},
+  },
+  dependencies: {
+    'react-native-gesture-handler': {
+      platforms: {
+        android: {},
+        ios: {},
+      },
+    },
+  },
+};
